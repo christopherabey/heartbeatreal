42e977cf3285dc3d5977cb9cb702aac13e5f3b30
Jayee <jayee.zheng@gmail.com>
2024-09-14 14:14:58 -0400

    initial

---
+++ backend/entry.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = "https://dbc-d3a357fd-4698.cloud.databricks.com/api/2.0/sql/statements/"
+headers = {
+  'Authorization': 'Bearer ' + os.getenv('DATABRICKS'),
+  'Content-Type': 'text/plain'
+}
+
+def add_entry(date, caption, image_data):
+    # INSERT INTO heartbeat.default.entries
+    # VALUES ('2024-09-14', 'Sample Caption', 'image_data');
+    payload = {}
+    payload['warehouse_id'] = '360d9dd238bf069f'
+    payload['statement'] = f"INSERT INTO heartbeat.default.entries VALUES ('{date}', '{caption}', '{image_data}');"
+    payload['wait_timeout'] = '30s'
+    response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+
+
+def get_entries(date: str):
+    # date format: '2024-09-14'
+    # Sample: SELECT * FROM heartbeat.default.entries WHERE date == '2024-09-14'
+    payload = {}
+    payload['warehouse_id'] = '360d9dd238bf069f'
+    payload['statement'] = f"SELECT * FROM heartbeat.default.entries WHERE date == '{date}'"
+    payload['wait_timeout'] = '30s'
+    response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+    return json.loads(response.text)
+    
+def start_heartbeat(heartrate: int):
+    # TODO: send notif 
+    pass
+
+if __name__ == '__main__':
+  pass
+    # add_entry('2024-09-14', 'Sample Caption', 'image_data')
+    # get_entries('2024-09-14')
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 from typing import Union
 
 from fastapi import FastAPI
+import entry
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/get_entries")
+def get_entries(date: str):
+   entry.get_entries(date)
+   return {"status": "success"}
+
+@app.post("/take_heartbeat")
+def take_heartbeat(heartrate: int):
+    entry.start_heartbeat(heartrate)
+    return {"status": "success"}
