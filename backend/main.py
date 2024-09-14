dabe312732f38134460083d3c02598fecfb365d7
Chris Abey <chris.abey587@gmail.com>
2024-09-14 14:26:29 -0400

    added port opening

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
-from typing import Union
-
+import os
 from fastapi import FastAPI
 import entry
 
@@ -18,3 +17,9 @@ def get_entries(date: str):
 def take_heartbeat(heartrate: int):
     entry.start_heartbeat(heartrate)
     return {"status": "success"}
+
+# To run the app with the correct port on Render
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Use the PORT environment variable provided by Render
+    uvicorn.run(app, host="0.0.0.0", port=port)
